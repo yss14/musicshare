a4235fd0b5240fc067a7086259f8b7b1955454fa
Yannick Stachelscheid <stachelscheid@addbots.de>
2019-03-20 21:34:30 +0100

    fix database client query return type

--- projects/cassandra-schema-builder/src/database-client.ts
+++ projects/cassandra-schema-builder/src/database-client.ts
@@ -1,8 +1,10 @@
 import * as cassandra from 'cassandra-driver';
 import { Columns, IQuery, TableRecord } from './table';
 
+type QueryRows<T> = (TableRecord<Extract<T, Columns>> & cassandra.types.Row)[];
+
 export interface IDatabaseClient {
-	query<T extends Columns>(query: IQuery<T>): Promise<TableRecord<Columns>[]>;
+	query<T extends Columns>(query: IQuery<T>): Promise<QueryRows<T>>;
 	execute(cql: string): Promise<void>;
 	close(): Promise<void>;
 }
@@ -12,10 +14,10 @@ export class DatabaseClient implements IDatabaseClient {
 		private readonly cassandraClient: cassandra.Client
 	) { }
 
-	public async query<T extends Columns>(query: IQuery<T>): Promise<TableRecord<Columns>[]> {
+	public async query<T extends Columns>(query: IQuery<T>): Promise<QueryRows<T>> {
 		const dbResult = await this.cassandraClient.execute(query.cql, query.values);
 
-		return dbResult.rows;
+		return dbResult.rows as any as QueryRows<T>;
 	}
 
 	public async execute(cql: string) {
