b2f7746bd17126062f04c6ca528f6f1cbcf5e8d4
Yannick Stachelscheid <yannickstach@googlemail.com>
2019-04-05 15:15:29 +0200

    chore: add eslint with ts support and react hook rules (#97)

---
+++ projects/frontend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module",
+		ecmaFeatures: {
+			modules: true
+		}
+	},
+	plugins: [
+		'@typescript-eslint',
+		'react-hooks'
+	],
+	rules: {
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
+	}
+}
