023d9e03210a0c9dfc9a568f26a4c9d16afbd521
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2019-04-10 11:27:37 +0200

    feature: frontend initial routing setup with share loading. (#120)

--- projects/frontend/src/pages/share/Share.tsx
+++ projects/frontend/src/pages/share/Share.tsx
@@ -1,5 +1,87 @@
 import React from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import { RouteComponentProps } from "react-router-dom";
 
-export default () => {
-  return <div />;
+interface IData {
+  share: {
+    songs: {
+      id: string;
+      title: string;
+      suffix: string;
+      year: number;
+      bpm: number;
+      dateLastEdit: string;
+      releaseDate: string;
+      isRip: boolean;
+      artists: string[];
+      remixer: string[];
+      featurings: string[];
+      type: string;
+      genres: string[];
+      label: string;
+      requiresUserAction: boolean;
+    }[];
+  };
+}
+
+interface IVariables {
+  id: string;
+}
+
+const GET_SHARE = gql`
+  query share($id: String!) {
+    share(id: $id) {
+      id
+      name
+      songs {
+        id
+        title
+        suffix
+        year
+        bpm
+        dateLastEdit
+        releaseDate
+        isRip
+        artists
+        remixer
+        featurings
+        type
+        genres
+        label
+        requiresUserAction
+      }
+    }
+  }
+`;
+
+interface IRouteProps {
+  id: string;
+}
+
+const Shares = ({ match }: RouteComponentProps<IRouteProps>) => {
+  const { id } = match.params;
+  return (
+    <>
+      <Query<IData, IVariables> query={GET_SHARE} variables={{ id }}>
+        {({ loading, error, data }) => {
+          if (loading) {
+            return <div>Loading ...</div>;
+          }
+          if (error) return `Error!: ${error}`;
+          if (data) {
+            return (
+              <ul>
+                {data.share.songs.map(el => (
+                  <li key={el.id}>{el.title}</li>
+                ))}
+              </ul>
+            );
+          }
+        }}
+      </Query>
+    </>
+  );
 };
+
+export default Shares;
