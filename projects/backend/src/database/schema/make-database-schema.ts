f67f02e7a14eb25b0c9bd556124e96842799a50f
Yannick Stachelscheid <stachelscheid@addbots.de>
2019-05-12 18:42:16 +0200

    create all tables from coretables definition

--- projects/backend/src/database/schema/make-database-schema.ts
+++ projects/backend/src/database/schema/make-database-schema.ts
@@ -1,11 +1,15 @@
 import { DatabaseSeed } from "../seed";
-import { UsersTable, SharesByUserTable, SongsByShareTable, SongTypesByShareTable, GenresByShareTable, UserLoginCredentialsTable, PlaylistsByShareTable, SongsByPlaylistTable, TokensByShareTable } from "./tables";
-import { IDatabaseClient, CQL } from "cassandra-schema-builder";
+import { /*UsersTable, SharesByUserTable, SongsByShareTable, SongTypesByShareTable, GenresByShareTable, UserLoginCredentialsTable, PlaylistsByShareTable, SongsByPlaylistTable, TokensByShareTable,*/ CoreTables } from "./tables";
+import { IDatabaseClient, CQL, Table } from "cassandra-schema-builder";
 import { Tables } from "./system-tables";
 
 export const makeDatabaseSchemaWithSeed = async (database: IDatabaseClient, seed: DatabaseSeed, opts: ICreateSchemaOpts) => {
+	const startTimeCreate = Date.now();
 	await makeDatabaseSchema(database, opts);
+	console.log(`Create ${Date.now() - startTimeCreate}ms`);
+	const startTimeSeed = Date.now();
 	await seed();
+	console.log(`Seed ${Date.now() - startTimeSeed}ms`);
 }
 
 interface ICreateSchemaOpts {
@@ -13,22 +17,18 @@ interface ICreateSchemaOpts {
 	clear?: boolean;
 }
 
+const keys = <O>(o: O) => {
+	return Object.keys(o) as (keyof O)[];
+}
+
 export const makeDatabaseSchema = async (database: IDatabaseClient, { keySpace, clear = false }: ICreateSchemaOpts) => {
 	if (clear) {
 		await clearKeySpace(database, keySpace);
 	}
 
-	await Promise.all([
-		database.execute(UsersTable.create().cql),
-		database.execute(SharesByUserTable.create().cql),
-		database.execute(SongsByShareTable.create().cql),
-		database.execute(SongTypesByShareTable.create().cql),
-		database.execute(GenresByShareTable.create().cql),
-		database.execute(UserLoginCredentialsTable.create().cql),
-		database.execute(PlaylistsByShareTable.create().cql),
-		database.execute(SongsByPlaylistTable.create().cql),
-		database.execute(TokensByShareTable.create().cql),
-	]);
+	const allTables = keys(CoreTables).map(name => Table(CoreTables, name));
+
+	await Promise.all(allTables.map(table => database.query(table.create())));
 }
 
 export const clearKeySpace = async (database: IDatabaseClient, keySpace: string): Promise<void> => {
