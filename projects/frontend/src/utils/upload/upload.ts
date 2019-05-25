156c89772bbf49b1320f1b3f09db757d5a8ff233
tobiKl <t.klesel@tum.de>
2019-05-25 20:55:54 +0200

    * add upload also to playlist when one is selected (#255)

--- projects/frontend/src/utils/upload/upload.ts
+++ projects/frontend/src/utils/upload/upload.ts
@@ -9,13 +9,14 @@ interface IAxiosProgress {
 export const upload = async (
 	userID: string,
 	shareID: string,
+	playlistIDs: string[],
 	file: File,
 	buffer: ArrayBuffer,
 	onProgress: (progress: IAxiosProgress) => void,
 	config: IConfig,
 ): Promise<void> => {
 	await axios.post<void>(
-		`http://127.0.0.1:4000/users/${userID}/shares/${shareID}/files/${file.name}`,
+		`http://127.0.0.1:4000/users/${userID}/shares/${shareID}/files/${file.name}?${playlistIDs.map(id => `playlistID=${id}`).join('&')}`,
 		buffer,
 		{
 			onUploadProgress: onProgress,
