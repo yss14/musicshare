f62fa8f70590ce4f3f0137d0db585bfc81fcb17d
Yannick Stachelscheid <yannickstach@googlemail.com>
2019-11-02 17:08:20 +0100

    handle NoScopesProvidedError and throw insuficcient permission error

--- projects/backend/src/auth/middleware/share-auth.ts
+++ projects/backend/src/auth/middleware/share-auth.ts
@@ -1,9 +1,10 @@
 import { Permissions } from "../permissions";
 import { Middleware } from "type-graphql/dist/interfaces/Middleware";
 import { IGraphQLContext } from "../../types/context";
-import { getShareIDFromRequest, hasAllPermissions, getCurrentPermissionsForShare } from "./auth-selectors";
+import { getShareIDFromRequest, hasAllPermissions, getCurrentPermissionsForShare, NoScopesProvidedError } from "./auth-selectors";
 import { UseMiddleware } from "type-graphql";
 import { MethodAndPropDecorator } from "type-graphql/dist/decorators/types";
+import { ForbiddenError } from "apollo-server-core";
 
 interface IShareAuthArgs {
 	permissions?: Permissions.Share[];
@@ -12,30 +13,38 @@ interface IShareAuthArgs {
 
 export const makeShareAuthMiddleware = ({ permissions, checkRef }: IShareAuthArgs): Middleware<IGraphQLContext> =>
 	async ({ args, root, context }, next) => {
-		const { services: { shareService }, scopes, userID } = context;
-		const shareID = getShareIDFromRequest({ root, args });
-		const shouldCheckShareRef = checkRef !== undefined ? checkRef : true;
+		try {
+			const { services: { shareService }, scopes, userID } = context;
+			const shareID = getShareIDFromRequest({ root, args });
+			const shouldCheckShareRef = checkRef !== undefined ? checkRef : true;
 
-		if (!shareID) {
-			throw new Error('Share reference not found');
-		}
-
-		if (shouldCheckShareRef) {
-			await shareService.getShareByID(shareID, userID!);
-		}
-
-		if (permissions) {
-			const isPermitted = hasAllPermissions(
-				permissions,
-				getCurrentPermissionsForShare(shareID, scopes)
-			);
-
-			if (!isPermitted) {
-				throw new Error(`User has insufficient permissions to perform this action!`);
+			if (!shareID) {
+				throw new Error('Share reference not found');
 			}
-		}
 
-		return next();
+			if (shouldCheckShareRef) {
+				await shareService.getShareByID(shareID, userID!);
+			}
+
+			if (permissions) {
+				const isPermitted = hasAllPermissions(
+					permissions,
+					getCurrentPermissionsForShare(shareID, scopes)
+				);
+
+				if (!isPermitted) {
+					throw new ForbiddenError(`User has insufficient permissions to perform this action!`);
+				}
+			}
+
+			return next();
+		} catch (err) {
+			if (err instanceof NoScopesProvidedError) {
+				throw new ForbiddenError(`User has insufficient permissions to perform this action!`);
+			}
+
+			throw err;
+		}
 	};
 
 type ShareAuth = {
--- projects/backend/src/auth/middleware/song-auth.ts
+++ projects/backend/src/auth/middleware/song-auth.ts
@@ -1,35 +1,44 @@
 import { Permissions } from "../permissions";
 import { Middleware } from "type-graphql/dist/interfaces/Middleware";
 import { IGraphQLContext } from "../../types/context";
-import { getShareIDFromRequest, hasAllPermissions, getCurrentPermissionsForShare, getSongIDFromRequest } from "./auth-selectors";
+import { getShareIDFromRequest, hasAllPermissions, getCurrentPermissionsForShare, getSongIDFromRequest, NoScopesProvidedError } from "./auth-selectors";
 import { UseMiddleware } from "type-graphql";
+import { ForbiddenError } from "apollo-server-core";
 
 export const makeSongAuthMiddleware = (permissions?: Permissions.Song[]): Middleware<IGraphQLContext> => async ({ args, root, context }, next) => {
-	const { services: { songService }, scopes } = context;
-	const songID = getSongIDFromRequest({ root, args });
-	const shareID = getShareIDFromRequest({ root, args });
+	try {
+		const { services: { songService }, scopes } = context;
+		const songID = getSongIDFromRequest({ root, args });
+		const shareID = getShareIDFromRequest({ root, args });
 
-	if (!songID) {
-		throw new Error('Song reference not found');
-	}
-	if (!shareID) {
-		throw new Error('Share reference not found');
-	}
-
-	await songService.getByID(shareID, songID);
-
-	if (permissions) {
-		const isPermitted = hasAllPermissions(
-			permissions,
-			getCurrentPermissionsForShare(shareID, scopes),
-		);
-
-		if (!isPermitted) {
-			throw new Error(`User has insufficient permissions to perform this action!`);
+		if (!songID) {
+			throw new Error('Song reference not found');
 		}
-	}
+		if (!shareID) {
+			throw new Error('Share reference not found');
+		}
 
-	return next();
+		await songService.getByID(shareID, songID);
+
+		if (permissions) {
+			const isPermitted = hasAllPermissions(
+				permissions,
+				getCurrentPermissionsForShare(shareID, scopes),
+			);
+
+			if (!isPermitted) {
+				throw new Error(`User has insufficient permissions to perform this action!`);
+			}
+		}
+
+		return next();
+	} catch (err) {
+		if (err instanceof NoScopesProvidedError) {
+			throw new ForbiddenError(`User has insufficient permissions to perform this action!`);
+		}
+
+		throw err;
+	}
 };
 
 export const SongAuth = (permissions?: Permissions.Song[]) => UseMiddleware(makeSongAuthMiddleware(permissions));
