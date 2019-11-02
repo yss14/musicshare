4c7e748d435c62ec8852b1404d21f35bc3a4564e
Yannick Stachelscheid <yannickstach@googlemail.com>
2019-11-02 16:52:47 +0100

    add playlist to simon's share; fix seed uuids

--- projects/backend/src/database/seed.ts
+++ projects/backend/src/database/seed.ts
@@ -16,7 +16,7 @@ import { Permissions } from '../auth/permissions';
 type Users = 'user1' | 'user2';
 type Shares = 'library_user1' | 'library_user2' | 'some_shared_library';
 type Songs = 'song1_library_user1' | 'song2_library_user1' | 'song3_library_user1';
-type Playlists = 'playlist1_library_user1' | 'playlist2_library_user1' | 'playlist_some_shared_library';
+type Playlists = 'playlist1_library_user1' | 'playlist2_library_user1' | 'playlist_some_shared_library' | 'playlist_library_user2';
 
 interface ITestDataSchema {
 	users: { [P in Users]: Required<IUserDBResult>; };
@@ -28,7 +28,7 @@ interface ITestDataSchema {
 export const testPassword = 'test1234';
 
 const songZeroOliverSmith: ISongDBResult = {
-	song_id: uuid(),
+	song_id: 'b5c143b5-0eb2-40d2-b098-8bd9a09a4492',
 	title: 'Zero',
 	suffix: null,
 	year: 2018,
@@ -51,7 +51,7 @@ const songZeroOliverSmith: ISongDBResult = {
 }
 
 const songPerthDusky: ISongDBResult = {
-	song_id: uuid(),
+	song_id: '7adb2fdc-35fa-4490-9d43-5b1a178902d8',
 	title: 'Perth',
 	suffix: null,
 	year: 2018,
@@ -74,7 +74,7 @@ const songPerthDusky: ISongDBResult = {
 }
 
 const songContactAlastor: ISongDBResult = {
-	song_id: uuid(),
+	song_id: 'a1cc61af-000e-49b2-86c6-6ff23060238b',
 	title: 'Contact',
 	suffix: null,
 	year: 2019,
@@ -96,11 +96,15 @@ const songContactAlastor: ISongDBResult = {
 	date_removed: null,
 }
 
-const libraryUser1ShareID = uuid();
-const libraryUser2ShareID = uuid();
-const someShareShareID = uuid();
+const libraryUser1ShareID = 'de35f11a-a748-49cc-8da2-02ef12109ea5';
+const libraryUser2ShareID = 'f02f540b-7db9-4655-b693-b89bb492a369';
+const someShareShareID = 'f9d531d3-94f0-4876-af17-deda34194345';
 const user1ID = 'f0d8e1f0-aeb1-11e8-a117-43673ffd376b';
 const user2ID = '3ba6fab4-f6ad-4916-9f1d-cdcfe522fd8e';
+const playlist1LibraryUser1ID = 'c3a21eb2-0cbd-4382-80b4-70925d6fd41d'
+const playlist2LibraryUser1ID = '76ba6247-079f-4700-a711-92a504704213'
+const playlistSomeSharedLibraryID = '8d6fa7cf-f416-408d-b964-d154e256a4cd'
+const playlist1LibraryUser2ID = '7d25fbd2-aaac-4730-b5a3-2e77e4166607'
 
 export const testData: ITestDataSchema = {
 	users: {
@@ -153,7 +157,7 @@ export const testData: ITestDataSchema = {
 	},
 	playlists: {
 		playlist1_library_user1: {
-			playlist_id: uuid(),
+			playlist_id: playlist1LibraryUser1ID,
 			name: 'Playlist 1',
 			date_removed: null,
 			songs: [songZeroOliverSmith, songPerthDusky, songContactAlastor],
@@ -161,7 +165,7 @@ export const testData: ITestDataSchema = {
 			share_id: libraryUser1ShareID,
 		},
 		playlist2_library_user1: {
-			playlist_id: uuid(),
+			playlist_id: playlist2LibraryUser1ID,
 			name: 'Playlist 2',
 			date_removed: null,
 			songs: [songZeroOliverSmith, songPerthDusky, songContactAlastor, songZeroOliverSmith, songPerthDusky, songContactAlastor],
@@ -169,12 +173,20 @@ export const testData: ITestDataSchema = {
 			share_id: libraryUser1ShareID,
 		},
 		playlist_some_shared_library: {
-			playlist_id: uuid(),
+			playlist_id: playlistSomeSharedLibraryID,
 			name: 'Some Shared Playlist 1',
 			date_removed: null,
 			songs: [songPerthDusky],
 			date_added: moment().subtract(3, 'hours').toDate(),
 			share_id: someShareShareID,
+		},
+		playlist_library_user2: {
+			playlist_id: playlist1LibraryUser2ID,
+			name: 'Playlist1 of Simon',
+			date_removed: null,
+			songs: [],
+			date_added: moment().subtract(3, 'hours').toDate(),
+			share_id: libraryUser2ShareID,
 		}
 	}
 }
