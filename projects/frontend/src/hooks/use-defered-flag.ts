1f66adfbad58c3d698304e035664466e4dff50b4
Yannick Stachelscheid <yannickstach@googlemail.com>
2019-11-12 10:33:15 +0100

    fix useDeferedFlag hook

--- projects/frontend/src/hooks/use-defered-flag.ts
+++ projects/frontend/src/hooks/use-defered-flag.ts
@@ -1,24 +1,24 @@
-import { useState, useRef } from "react"
+import { useState, useRef, useCallback } from "react"
 
 export const useDeferedFlag = (delay: number) => {
 	const timeoutRef = useRef<number | null>(null)
 	const [value, setValue] = useState(false)
 
-	const toggle = () => {
+	const toggle = useCallback(() => {
 		if (timeoutRef.current) {
 			clearTimeout(timeoutRef.current)
 		}
 
 		timeoutRef.current = setTimeout(() => setValue(true), delay)
-	}
+	}, [timeoutRef, setValue, delay])
 
-	const reset = () => {
+	const reset = useCallback(() => {
 		if (timeoutRef.current) {
 			clearTimeout(timeoutRef.current)
 		}
 
 		setValue(false)
-	}
+	}, [timeoutRef, setValue])
 
 	return [value, toggle, reset] as [boolean, () => void, () => void]
 }
