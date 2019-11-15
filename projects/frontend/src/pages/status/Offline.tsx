c8dc30adc2770959f28e23f45aac2b93594455c7
Yannick Stachelscheid <yannickstach@googlemail.com>
2019-11-15 17:22:24 +0100

    add offline status page

---
+++ projects/frontend/src/pages/status/Offline.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom';
+import offlineImg from '../../images/offline.png'
+
+const OfflineContainer = styled.div`
+	width: 100%;
+	padding: 20px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const Image = styled.img`
+	width: 200px;
+	height: 200px;
+`
+
+export const Offline = () => (
+	<OfflineContainer>
+		<Image src={offlineImg} alt="offline" />
+		<h2>Offline?</h2>
+		<p></p>
+		<Link to="/">Retry</Link>
+	</OfflineContainer>
+)
