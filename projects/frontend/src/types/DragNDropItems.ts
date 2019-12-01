c6159c61643bb070d966282b90b76ac8b70e6a0e
Yannick Stachelscheid <yannickstach@googlemail.com>
2019-12-01 22:39:41 +0100

    move dnd song add playlist logic to playlist sidebar item

--- projects/frontend/src/types/DragNDropItems.ts
+++ projects/frontend/src/types/DragNDropItems.ts
@@ -1,3 +1,10 @@
+import { IScopedSong } from "../graphql/types";
+
 export enum DragNDropItem {
 	Song = 'song'
+}
+
+export interface IAcceptSong {
+	type: DragNDropItem.Song;
+	song: IScopedSong;
 }
