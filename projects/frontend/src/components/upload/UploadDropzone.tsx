0fe786a5f9db5de2d23fc1643f627509e6715222
Yannick Stachelscheid <yannick.stachelscheid@tum.de>
2020-04-27 13:55:40 +0200

    upload round progress and group by progress > 0

--- projects/frontend/src/components/upload/UploadDropzone.tsx
+++ projects/frontend/src/components/upload/UploadDropzone.tsx
@@ -3,6 +3,7 @@ import Dropzone from "./Dropzone"
 import { Flex, Box } from "../Flex"
 import { Progress } from "antd"
 import styled from "styled-components"
+import { groupBy } from "lodash"
 
 const UploadProgressContainer = styled(Box)`
 	background-color: white;
@@ -26,18 +27,25 @@ export const UploadDropzone: React.FC = ({ children }) => (
 				<Flex direction="column" style={{ width: "100%", height: "100%" }}>
 					{uploadItems.length > 0 ? (
 						<UploadProgressContainer>
-							{uploadItems.map((item, idx) => (
-								<UploadItem key={idx + item.hash}>
-									<div>{item.filename}</div>
-									<Progress
-										percent={item.progress}
-										showInfo={true}
-										status={
-											item.status === 2 ? (item.status === 3 ? "exception" : "success") : "active"
-										}
-									/>
-								</UploadItem>
-							))}
+							{uploadItems
+								.filter((item) => item.progress > 0)
+								.concat(uploadItems.filter((item) => item.progress === 0))
+								.map((item, idx) => (
+									<UploadItem key={idx + item.hash}>
+										<div>{item.filename}</div>
+										<Progress
+											percent={Math.round(item.progress)}
+											showInfo={true}
+											status={
+												item.status === 2
+													? item.status === 3
+														? "exception"
+														: "success"
+													: "active"
+											}
+										/>
+									</UploadItem>
+								))}
 						</UploadProgressContainer>
 					) : null}
 					<Box style={{ width: "100%", height: "100%" }}>
