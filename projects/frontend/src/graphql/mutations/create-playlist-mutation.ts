1a386f7ea316d792dde2f31621d2ebb2e006b806
Yannick Stachelscheid <yannick.stachelscheid@tum.de>
2020-05-13 22:33:46 +0200

    merged view add create playlist button

--- projects/frontend/src/graphql/mutations/create-playlist-mutation.ts
+++ projects/frontend/src/graphql/mutations/create-playlist-mutation.ts
@@ -10,6 +10,7 @@ import { MutationUpdaterFn } from "apollo-client/core/watchQueryOptions"
 import { IPlaylist } from "../types"
 import { useCallback } from "react"
 import { MutationResult } from "react-apollo"
+import { IGetMergedPlaylistData, GET_MERGED_PLAYLISTS } from "../queries/merged-playlists-query"
 
 export interface ICreatePlaylistVariables {
 	shareID: string
@@ -30,23 +31,53 @@ export const CREATE_PLAYLIST = gql`
 
 export const useCreatePlaylist = (opts?: MutationHookOptions<ICreatePlaylistData, ICreatePlaylistVariables>) => {
 	const makeUpdatePlaylistCache = useCallback(
-		(shareID: string): MutationUpdaterFn<ICreatePlaylistData> => (cache, { data }) => {
-			const currentPlaylists = cache.readQuery<IGetPlaylistsData, IGetPlaylistsVariables>({
-				query: GET_SHARE_PLAYLISTS,
-				variables: { shareID },
-			})!.share.playlists
+		(shareID: string, isMergedView: boolean): MutationUpdaterFn<ICreatePlaylistData> => (cache, { data }) => {
+			if (!isMergedView) {
+				const sharePlaylistsQuery = cache.readQuery<IGetPlaylistsData, IGetPlaylistsVariables>({
+					query: GET_SHARE_PLAYLISTS,
+					variables: { shareID },
+				})
 
-			cache.writeQuery<IGetPlaylistsData, IGetPlaylistsVariables>({
-				query: GET_SHARE_PLAYLISTS,
-				data: {
-					share: {
-						id: shareID,
-						__typename: "Share",
-						playlists: currentPlaylists.concat([data!.createPlaylist]),
-					},
-				},
-				variables: { shareID },
-			})
+				if (sharePlaylistsQuery && data) {
+					const sharePlaylists = sharePlaylistsQuery.share.playlists
+
+					cache.writeQuery<IGetPlaylistsData, IGetPlaylistsVariables>({
+						query: GET_SHARE_PLAYLISTS,
+						data: {
+							share: {
+								id: shareID,
+								__typename: "Share",
+								playlists: sharePlaylists.concat([data.createPlaylist]),
+							},
+						},
+						variables: { shareID },
+					})
+				}
+			} else {
+				const mergedPlaylistsQuery = cache.readQuery<IGetMergedPlaylistData, void>({
+					query: GET_MERGED_PLAYLISTS,
+				})
+
+				if (mergedPlaylistsQuery) {
+					cache.writeQuery<IGetMergedPlaylistData, void>({
+						query: GET_MERGED_PLAYLISTS,
+						data: {
+							...mergedPlaylistsQuery,
+							viewer: {
+								...mergedPlaylistsQuery.viewer,
+								shares: mergedPlaylistsQuery.viewer.shares.map((share) =>
+									share.id === data?.createPlaylist.shareID
+										? {
+												...share,
+												playlists: share.playlists.concat(data.createPlaylist),
+										  }
+										: share,
+								),
+							},
+						},
+					})
+				}
+			}
 		},
 		[],
 	)
@@ -54,17 +85,20 @@ export const useCreatePlaylist = (opts?: MutationHookOptions<ICreatePlaylistData
 	const [createPlaylistMutation, other] = useMutation(CREATE_PLAYLIST, opts)
 
 	const createPlaylist = useCallback(
-		(shareID: string, name: string) => {
+		(shareID: string, name: string, isMergedView: boolean) => {
 			createPlaylistMutation({
 				variables: {
 					shareID,
 					name,
 				},
-				update: makeUpdatePlaylistCache(shareID),
+				update: makeUpdatePlaylistCache(shareID, isMergedView),
 			})
 		},
 		[createPlaylistMutation, makeUpdatePlaylistCache],
 	)
 
-	return [createPlaylist, other] as [(shareID: string, name: string) => void, MutationResult<ICreatePlaylistData>]
+	return [createPlaylist, other] as [
+		(shareID: string, name: string, isMergedView: boolean) => void,
+		MutationResult<ICreatePlaylistData>,
+	]
 }
