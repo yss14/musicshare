10b7aed9872322becbf633d7bda37a5cc2f98dd2
Yannick Stachelscheid <yannickstach@googlemail.com>
2020-05-13 20:14:00 +0200

    fix: player auto play next song (#1578)

--- projects/frontend/src/player/player-hook.ts
+++ projects/frontend/src/player/player-hook.ts
@@ -2,15 +2,13 @@ import { useCallback, useMemo } from "react"
 import { IScopedSong } from "../graphql/types"
 import {
 	makeUpdatePlayerState,
-	usePlayerState,
 	IPlayerQueueItem,
 	useSetPlayerQueue,
 	usePlayerQueueState,
 } from "../components/player/player-state"
 import { useApolloClient } from "react-apollo"
-import { makeGetSongMediaUrls } from "../graphql/programmatic/get-song-mediaurl"
 import { v4 as uuid } from "uuid"
-import { usePlayerContext, pickMediaUrl } from "./PlayerContext"
+import { usePlayerContext } from "./PlayerContext"
 
 const QueueItem = (song: IScopedSong): IPlayerQueueItem => ({
 	__typename: "PlayerQueueItem",
@@ -19,115 +17,7 @@ const QueueItem = (song: IScopedSong): IPlayerQueueItem => ({
 })
 
 export const usePlayerActions = () => {
-	const {
-		primaryDeck,
-		bufferingDeck,
-		setIsBufferingNextSong,
-		setPlayCountIncremented,
-		playedSongs,
-		setPlayedSongs,
-	} = usePlayerContext()
-	const { data } = usePlayerState()
-	const { currentSong, queue } = data!.player
-
-	const client = useApolloClient()
-
-	const updatePlayerState = useMemo(() => makeUpdatePlayerState(client), [client])
-	const getMediaUrls = useMemo(() => makeGetSongMediaUrls(client), [client])
-
-	const next = useCallback(
-		async (song?: IScopedSong) => {
-			if (currentSong) {
-				setPlayedSongs((currentPlayedSongs) => [...currentPlayedSongs, currentSong])
-			}
-			let nextSong = song
-
-			if (!nextSong) {
-				const newQueue = [...queue]
-				const nextItem = newQueue.shift()
-				nextSong = nextItem?.song
-
-				updatePlayerState({
-					queue: newQueue,
-				})
-			}
-
-			setIsBufferingNextSong(false)
-
-			bufferingDeck.src = ""
-			primaryDeck.setAttribute("src", "")
-
-			if (!nextSong) {
-				updatePlayerState({
-					currentSong: null,
-					playing: false,
-				})
-
-				return false
-			}
-
-			const songMediaUrls = await getMediaUrls(nextSong.shareID, nextSong.id)
-
-			updatePlayerState({
-				currentSong: nextSong,
-				error: null,
-			})
-
-			const mediaUrl = pickMediaUrl(songMediaUrls)
-
-			if (mediaUrl) {
-				primaryDeck.src = mediaUrl
-				primaryDeck.play()
-
-				setPlayCountIncremented(false)
-			} else {
-				console.warn(`Cannot get a media url of song ${nextSong.id}`)
-
-				await next()
-			}
-		},
-		[
-			currentSong,
-			getMediaUrls,
-			queue,
-			updatePlayerState,
-			bufferingDeck,
-			primaryDeck,
-			setIsBufferingNextSong,
-			setPlayCountIncremented,
-			setPlayedSongs,
-		],
-	)
-
-	const prev = useCallback(async () => {
-		const newLastPlayedSongs = [...playedSongs]
-		const prevSong = newLastPlayedSongs.pop()
-		setPlayedSongs(newLastPlayedSongs)
-
-		if (!prevSong) {
-			return
-		}
-
-		const songMediaUrls = await getMediaUrls(prevSong.shareID, prevSong.id)
-
-		updatePlayerState({
-			currentSong: prevSong,
-			error: null,
-		})
-
-		const mediaUrl = pickMediaUrl(songMediaUrls)
-
-		if (mediaUrl) {
-			primaryDeck.src = mediaUrl
-			primaryDeck.play()
-
-			setPlayCountIncremented(false)
-		} else {
-			console.warn(`Cannot get a media url of song ${prevSong.id}`)
-
-			await prev()
-		}
-	}, [getMediaUrls, playedSongs, primaryDeck, setPlayCountIncremented, setPlayedSongs, updatePlayerState])
+	const { primaryDeck, next, prev } = usePlayerContext()
 
 	const changeSong = useCallback(
 		async (newSong: IScopedSong) => {
