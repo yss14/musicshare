6f4c1b6b55b294b559c515601731d44f43b78536
Yannick Stachelscheid <yannick.stachelscheid@tum.de>
2020-09-11 11:59:53 +0200

    add threshould arg to useSongNearDuplicates()

--- projects/react-graphql-client/src/remote/mutations/useSongNearDuplicates.ts
+++ projects/react-graphql-client/src/remote/mutations/useSongNearDuplicates.ts
@@ -12,6 +12,7 @@ export interface IFindSongNearDuplicatesData {
 export interface IFindSongNearDuplicatesVariables {
 	title: string
 	artist: string
+	threshould?: number
 }
 
 export const GET_SONG_NEAR_DUPLICATES = TransformedGraphQLMutation<
