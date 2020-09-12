e7e90098226f66b1a4d254c5c4185b737494836b
Yannick Stachelscheid <yannick.stachelscheid@tum.de>
2020-09-12 17:37:49 +0200

    add song type mutation hooks

---
+++ projects/react-graphql-client/src/remote/mutations/useRemoveSongType.ts
@@ -0,0 +1,37 @@
+import gql from "graphql-tag"
+import {
+	TransformedGraphQLMutation,
+	IGraphQLMutationOpts,
+	useGraphQLMutation,
+	typedQueryCache,
+} from "../../react-query-graphql"
+import { GET_SONGTYPES } from "../queries/useSongTypes"
+
+export interface IRemoveSongTypeData {
+	removeSongType: boolean
+}
+
+export interface IRemoveSongTypeVariables {
+	songTypeID: string
+}
+
+export const REMOVE_SONG_TYPE = TransformedGraphQLMutation<IRemoveSongTypeData, IRemoveSongTypeVariables>(gql`
+	mutation removeSongType($songTypeID: String!) {
+		removeSongType(songTypeID: $songTypeID)
+	}
+`)((data) => data.removeSongType)
+
+export const useRemoveSongType = (opts?: IGraphQLMutationOpts<typeof REMOVE_SONG_TYPE>) => {
+	const mutation = useGraphQLMutation(REMOVE_SONG_TYPE, {
+		...opts,
+		onSuccess: (data, variables) => {
+			typedQueryCache.invalidateTypedQuery({
+				query: GET_SONGTYPES,
+			})
+
+			if (opts?.onSuccess) opts.onSuccess(data, variables)
+		},
+	})
+
+	return mutation
+}
---
+++ projects/react-graphql-client/src/remote/mutations/useUpdateSongType.ts
@@ -0,0 +1,47 @@
+import gql from "graphql-tag"
+import {
+	TransformedGraphQLMutation,
+	IGraphQLMutationOpts,
+	useGraphQLMutation,
+	typedQueryCache,
+} from "../../react-query-graphql"
+import { SongType, songTypeKeys } from "@musicshare/shared-types"
+import { GET_SONGTYPES } from "../queries/useSongTypes"
+
+export interface IUpdateSongTypeData {
+	updateSongType: SongType
+}
+
+export interface IUpdateSongTypeVariables {
+	songTypeID: string
+	name: string
+	group: string
+	alternativeNames: string[]
+	hasArtists: boolean
+}
+
+export const UPDATE_SONG_TYPE = TransformedGraphQLMutation<IUpdateSongTypeData, IUpdateSongTypeVariables>(gql`
+	mutation updateSongType($songTypeID: String! $genreID: String! $name: String! $group: String! $alternativeNames: [String!]! $hasArtists: Boolean!) {
+		updateSongType(songTypeID: $songTypeID genreID: $genreID name: $name group: $group alternativeNames: $alternativeNames hasArtists: $hasArtists){
+			${songTypeKeys}
+		}
+	}
+`)((data) => data.updateSongType)
+
+export const useUpdateSongType = (opts?: IGraphQLMutationOpts<typeof UPDATE_SONG_TYPE>) => {
+	const mutation = useGraphQLMutation(UPDATE_SONG_TYPE, {
+		...opts,
+		onSuccess: (data, variables) => {
+			typedQueryCache.setTypedQueryData(
+				{
+					query: GET_SONGTYPES,
+				},
+				(currentData) => (currentData || []).map((songType) => (songType.id === data.id ? data : songType)),
+			)
+
+			if (opts?.onSuccess) opts.onSuccess(data, variables)
+		},
+	})
+
+	return mutation
+}
