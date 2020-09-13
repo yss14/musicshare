a91aa3c5e1f7de8df42b24fe8c82bd653bb54c81
Yannick Stachelscheid <yannick.stachelscheid@tum.de>
2020-09-13 16:45:20 +0200

    update dequal

--- projects/frontend/src/hooks/use-deep-compare-effect.ts
+++ projects/frontend/src/hooks/use-deep-compare-effect.ts
@@ -1,12 +1,12 @@
 import { useEffect, useRef } from "react"
-import deepEqual from "dequal"
+import { dequal } from "dequal"
 
 // taken from https://github.com/kentcdodds/use-deep-compare-effect
 
 const useDeepCompareMemoize = (value?: React.DependencyList) => {
 	const ref = useRef<React.DependencyList>()
 
-	if (!deepEqual(value, ref.current)) {
+	if (!dequal(value, ref.current)) {
 		ref.current = value
 	}
 
