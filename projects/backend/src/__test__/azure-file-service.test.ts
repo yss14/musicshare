30cc7209ff0e4a81cd6c33342e5e6dc6a2e65a65
Yannick Stachelscheid <yannickstach@googlemail.com>
2021-03-07 10:36:53 +0100

    enable file url test cases again for local environments

--- projects/backend/src/__test__/azure-file-service.test.ts
+++ projects/backend/src/__test__/azure-file-service.test.ts
@@ -123,106 +123,102 @@ describe("file upload", () => {
 	)
 })
 
-// These test cases only work with real-world azure blob at the moment
-// https://github.com/Azure/Azurite/issues/151
-if (process.env.IS_CI) {
-	describe("get url to file", () => {
-		const mp3FilePath = path.join(__dirname, "assets", "SampleAudio.mp3")
-		const container = "testupload"
+describe("get url to file", () => {
+	const mp3FilePath = path.join(__dirname, "assets", "SampleAudio.mp3")
+	const container = "testupload"
 
-		test("get url to uploaded file", async () => {
-			const fileService = new AzureFileService(container)
-			await fileService.createContainerIfNotExists()
-			const filenameRemote = "SomeFile.mp3"
+	test("get url to uploaded file", async () => {
+		const fileService = new AzureFileService(container)
+		await fileService.createContainerIfNotExists()
+		const filenameRemote = "SomeFile.mp3"
 
-			await fileService.uploadFile({
-				filenameRemote: filenameRemote,
-				contentType: "audio/mp3",
-				source: fs.createReadStream(mp3FilePath),
-			})
-
-			const urlToFile = await fileService.getLinkToFile({
-				filenameRemote,
-				expireDate: moment().add(20, "seconds"),
-			})
-
-			const urlToFileIsReachable = await urlIsReachable(urlToFile)
-
-			expect(urlToFileIsReachable).toBeTruthy()
+		await fileService.uploadFile({
+			filenameRemote: filenameRemote,
+			contentType: "audio/mp3",
+			source: fs.createReadStream(mp3FilePath),
 		})
 
-		test("get uploadable url", async () => {
-			const fileService = new AzureFileService(container)
-			const filenameRemote = "SomeFile.mp3"
-
-			const urlToFile = await fileService.getLinkToFile({
-				filenameRemote,
-				expireDate: moment().add(20, "seconds"),
-				permission: "write",
-			})
-
-			expect(urlToFile).toBeString()
+		const urlToFile = await fileService.getLinkToFile({
+			filenameRemote,
+			expireDate: moment().add(20, "seconds"),
 		})
 
-		test("get url to uploaded file expired", async () => {
-			const fileService = new AzureFileService(container)
-			await fileService.createContainerIfNotExists()
-			const filenameRemote = "SomeFile.mp3"
+		const urlToFileIsReachable = await urlIsReachable(urlToFile)
 
-			await fileService.uploadFile({
-				filenameRemote: filenameRemote,
-				contentType: "audio/mp3",
-				source: fs.createReadStream(mp3FilePath),
-			})
-
-			const urlToFile = await fileService.getLinkToFile({
-				filenameRemote,
-				expireDate: moment().add(-20, "seconds"),
-			})
-
-			const urlToFileIsReachable = await urlIsReachable(urlToFile)
-
-			expect(urlToFileIsReachable).toBeFalsy()
-		})
-
-		test("get url to uploaded file no end date specified", async () => {
-			const fileService = new AzureFileService(container)
-			await fileService.createContainerIfNotExists()
-			const filenameRemote = "SomeFile.mp3"
-
-			await fileService.uploadFile({
-				filenameRemote: filenameRemote,
-				contentType: "audio/mp3",
-				source: fs.createReadStream(mp3FilePath),
-			})
-
-			const urlToFile = await fileService.getLinkToFile({ filenameRemote })
-
-			await new Promise<void>((resolve) => setTimeout(() => resolve(), 1000))
-
-			const urlToFileIsReachable = await urlIsReachable(urlToFile)
-
-			expect(urlToFileIsReachable).toBeFalsy()
-		})
-
-		test("get url with browser host specified", async () => {
-			const browserHost = "http://localhost:1337"
-			const fileService = new AzureFileService(container, undefined, browserHost)
-			await fileService.createContainerIfNotExists()
-			const filenameRemote = "SomeFile.mp3"
-
-			await fileService.uploadFile({
-				filenameRemote: filenameRemote,
-				contentType: "audio/mp3",
-				source: fs.createReadStream(mp3FilePath),
-			})
-
-			const urlToFile = await fileService.getLinkToFile({ filenameRemote })
-
-			expect(urlToFile).toMatch(new RegExp(browserHost))
-		})
+		expect(urlToFileIsReachable).toBeTruthy()
 	})
-}
+
+	test("get uploadable url", async () => {
+		const fileService = new AzureFileService(container)
+		const filenameRemote = "SomeFile.mp3"
+
+		const urlToFile = await fileService.getLinkToFile({
+			filenameRemote,
+			expireDate: moment().add(20, "seconds"),
+			permission: "write",
+		})
+
+		expect(urlToFile).toBeString()
+	})
+
+	test("get url to uploaded file expired", async () => {
+		const fileService = new AzureFileService(container)
+		await fileService.createContainerIfNotExists()
+		const filenameRemote = "SomeFile.mp3"
+
+		await fileService.uploadFile({
+			filenameRemote: filenameRemote,
+			contentType: "audio/mp3",
+			source: fs.createReadStream(mp3FilePath),
+		})
+
+		const urlToFile = await fileService.getLinkToFile({
+			filenameRemote,
+			expireDate: moment().add(-20, "seconds"),
+		})
+
+		const urlToFileIsReachable = await urlIsReachable(urlToFile)
+
+		expect(urlToFileIsReachable).toBeFalsy()
+	})
+
+	test("get url to uploaded file no end date specified", async () => {
+		const fileService = new AzureFileService(container)
+		await fileService.createContainerIfNotExists()
+		const filenameRemote = "SomeFile.mp3"
+
+		await fileService.uploadFile({
+			filenameRemote: filenameRemote,
+			contentType: "audio/mp3",
+			source: fs.createReadStream(mp3FilePath),
+		})
+
+		const urlToFile = await fileService.getLinkToFile({ filenameRemote })
+
+		await new Promise<void>((resolve) => setTimeout(() => resolve(), 1000))
+
+		const urlToFileIsReachable = await urlIsReachable(urlToFile)
+
+		expect(urlToFileIsReachable).toBeFalsy()
+	})
+
+	test("get url with browser host specified", async () => {
+		const browserHost = "http://localhost:1337"
+		const fileService = new AzureFileService(container, undefined, browserHost)
+		await fileService.createContainerIfNotExists()
+		const filenameRemote = "SomeFile.mp3"
+
+		await fileService.uploadFile({
+			filenameRemote: filenameRemote,
+			contentType: "audio/mp3",
+			source: fs.createReadStream(mp3FilePath),
+		})
+
+		const urlToFile = await fileService.getLinkToFile({ filenameRemote })
+
+		expect(urlToFile).toMatch(new RegExp(browserHost))
+	})
+})
 
 describe("get file as buffer", () => {
 	const mp3FilePath = path.join(__dirname, "assets", "SampleAudio.mp3")
